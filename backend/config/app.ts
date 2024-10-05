1b8e2368f760a6fb87808fcb052d99a55a5422ab
sk-phan <sukiphan97@gmail.com>
2024-10-05 20:15:55 +0300

    chore (backend-setup): change file path and add gitignore file

--- backend/config/app.ts
+++ backend/config/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import logger from "./logger";
 import config from "./config";
+import { errorHandler } from '../middlewares/errorHandler.middleware';
+import userRoutes from '../routes/userRoutes';
 
 const cors = require('cors');
 const app = express();
@@ -22,5 +24,10 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+// Must be defined in the end of application
+app.use(errorHandler); 
 
 export default app;
--- backend/config/config.ts
+++ backend/config/config.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const PORT: number = Number(process.env.PORT) || 3003;
 const isProduction: boolean = process.env.NODE_ENV === 'production';
-const localMongoURI: string = "mongodb://localhost:27017/care-app";
+const localMongoURI: string = "mongodb://0.0.0.0:27017/care-app";
 const productionMongoURI: string = "mongodb+srv://sukiphan97:TZsHfxyPeHhilRRo@cluster0.ocs4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const MONGODB_URI: string = isProduction ? productionMongoURI : localMongoURI;
