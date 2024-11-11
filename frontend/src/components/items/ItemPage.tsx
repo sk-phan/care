53e0a35a41d9102d242c8ae5ba6148f6eab1e3c4
sk-phan <sukiphan97@gmail.com>
2024-11-11 23:18:15 +0200

    feat (item): improve layout

--- frontend/src/components/items/ItemPage.tsx
+++ frontend/src/components/items/ItemPage.tsx
@@ -1,8 +1,12 @@
 "use client"
 import { useEffect } from "react";
-import "../../styles/ItemPage.css";
-import ItemContactForm from "./forms/item-contact-form";
 import { ItemType } from "@/types/items.type";
+
+import ItemContactForm from "./forms/item-contact-form";
+import "../../styles/ItemPage.css";
+import Image from "next/image";
+
+const _ = require('lodash'); 
 
 const ItemPage = ({
     item
@@ -22,23 +26,28 @@ const ItemPage = ({
 
     return (
         <div className="flex flex-col md:flex-row">
-            <img 
+            <Image 
             id="item-image"
             src={item.image} 
             alt="Item image"
+            width={100}
+            height={100}
+            unoptimized
             className="rounded-xl w-full md:w-1/2 object-cover item-image"
             />
             <div className="mt-6 md:mt-0 md:ml-8 md:w-1/2 h-fit" id="item-info">
+                <span className="bg-green-500 text-white w-fit px-4 py-1 rounded-full">
+                    {_.capitalize(item.status)}
+                </span>
                 <h3
-                className="text-2xl md:text-4xl font-medium break-words"
+                className="text-2xl md:text-4xl mt-4 mb-2 font-medium break-words"
                 >
                     {item.title}
                 </h3>
-                <div className="flex flex-col mt-6 md:mt-8 mb-6">
-                    <span className="bg-green-500 mb-2 text-white w-fit px-4 py-1 rounded-full">{item.status}</span>
+                <div className="flex flex-col mb-1">
                     <span>
-                        <span className="text-gray-500">Status:</span> 
-                        <span className="ml-2 font-medium">{item.condition}</span>
+                        <span className="text-gray-500">Condition:</span> 
+                        <span className="ml-2 font-medium">{_.capitalize(item.condition)}</span>
                     </span>
                     <span>
                         <span className="text-gray-500">Location:</span>  
