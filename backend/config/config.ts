bb2f3136ce264d2e4ad2464bfee83a2f4a05f23e
sk-phan <sukiphan97@gmail.com>
2024-12-23 21:26:29 +0200

    chore: remove local database to test

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT: number = Number(process.env.PORT) || 3000;
-const isProduction: boolean = process.env.NODE_ENV === 'production';
-const localMongoURI: string = "mongodb://0.0.0.0:27017/care-app";
+// const isProduction: boolean = process.env.NODE_ENV === 'production';
+// const localMongoURI: string = "mongodb://0.0.0.0:27017/care-app";
 const productionMongoURI = process.env.MONGODB_URI;
 
-const MONGODB_URI = isProduction ? productionMongoURI : localMongoURI;
+const MONGODB_URI = productionMongoURI;
 
 const config = { MONGODB_URI, PORT };
 export default config;
