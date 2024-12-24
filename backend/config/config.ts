a23498649581fedb653b88ccfbf6aa7216ebee13
sk-phan <sukiphan97@gmail.com>
2024-12-24 14:06:33 +0200

    chore: update package json scripts

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT: number = 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 // const isProduction: boolean = process.env.NODE_ENV === 'production';
 // const localMongoURI: string = "mongodb://0.0.0.0:27017/care-app";
 const productionMongoURI = process.env.MONGODB_URI;
