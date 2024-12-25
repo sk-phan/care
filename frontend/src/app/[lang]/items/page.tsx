ec1e29dd90171659f3e0d6ec48acb04f6970cf12
sk-phan <sukiphan97@gmail.com>
2024-12-25 23:11:05 +0200

    chore: install @netlify/plugin-nextjs

--- frontend/src/app/[lang]/items/page.tsx
+++ frontend/src/app/[lang]/items/page.tsx
@@ -3,7 +3,7 @@ import { BASE_URL } from "@/services/api-base";
 import { ItemType } from "@/types/item/item.type";
 
 export default async function Items({ params } : {params: { lang: string };}) {
-    const data = await fetch(BASE_URL + '/items', {cache: "no-cache"});
+    const data = await fetch(BASE_URL + '/items');
     const items: ItemType[] = await data.json();
     const lang = params.lang;
     
