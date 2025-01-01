c3a7e09871d0c7ee247b70ead4f7fc874c245695
sk-phan <sukiphan97@gmail.com>
2025-01-01 19:21:05 +0200

    hide api call

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -1,24 +1,23 @@
 import HeroSection from '@/components/hero-section/hero-section';
 import AboutSection from '@/components/about/about-section';
 
-import { BASE_URL } from '@/services/api-base';
+// import { BASE_URL } from '@/services/api-base';
 
 import { LocaleType } from '../i18n/locales/locales.type';
 
-const PAGE = 1;
-const LIMIT = 6;
+// const PAGE = 1;
+// const LIMIT = 6;
 
 export default async function Home({ params } : {params: { lang: LocaleType };}) {
   const lang = params.lang;
 
-  const response = await fetch(BASE_URL + `/items?page=${PAGE}&limit=${LIMIT}`, {cache: "no-store"});
-  const data = await response.json();
+  // const response = await fetch(BASE_URL + `/items?page=${PAGE}&limit=${LIMIT}`, {cache: "no-store"});
+  // const data = await response.json();
 
   // const items: ItemType[] = data.entities || [];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      {JSON.stringify(data) }
       <HeroSection
       lang={lang}/>
       <AboutSection 
