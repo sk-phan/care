e48e8813841a9449c9baca88aa9410a58cc5f449
sk-phan <sukiphan97@gmail.com>
2025-01-02 22:24:31 +0200

    redeploy backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,13 +2,4 @@ module.exports = {
     images: {
         domains: ['images.unsplash.com', 'plus.unsplash.com'], 
     },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/en',
-                permanent: true,
-            },
-        ]
-    },
 }
