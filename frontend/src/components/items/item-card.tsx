136d32f9ef3f70b93c4c12a263d26755a8faaa42
sk-phan <sukiphan97@gmail.com>
2025-01-26 13:51:19 +0200

    chore: fix image path broken

--- frontend/src/components/items/item-card.tsx
+++ frontend/src/components/items/item-card.tsx
@@ -1,17 +1,16 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
 import _ from 'lodash';
 
 import { urlConfigs } from "@/routing/url-configs";
 import { ItemType } from "@/types/item/item.type";
 
 import Badge from "@/components/common/c-badge";
-import placeholderImage from "../../../../public/images/item-placeholder-img.png";
 
 import "@/styles/Item.css";
 import { useTranslation } from "@/app/i18n";
 import useLocale from "@/app/i18n/use-locale";
+import Image from "next/image";
 
 const ItemCard = ({ item } : { item: ItemType }) => {
     const itemPath = urlConfigs.Item;
@@ -33,7 +32,8 @@ const ItemCard = ({ item } : { item: ItemType }) => {
                 sizes="100vw"
                 style={{ width: '100%', height: '270px'}} 
                 alt="item"
-                src={item.image || placeholderImage.src}/>
+                src={item.image ? item.image : "/images/item.png"} 
+                />
             </div>
             <div className="flex flex-col">
                 <h3 className="text-lg font-semibold">{item.title}</h3>
