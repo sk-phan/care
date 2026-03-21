91314c1bc086d8fa77cefcac8cc956e3096d0def
sk-phan <sukiphan97@gmail.com>
2026-03-21 15:15:16 +0200

    fix: update MongoDB connection string in config

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -3,9 +3,9 @@ dotenv.config();
 
 const PORT: number = Number(process.env.PORT) || 3000;
 const isProduction: boolean = process.env.NODE_ENV === 'production';
-const localMongoURI = process.env.LOCAL_MONGO_URI;
 
-const MONGODB_URI = isProduction ?  process.env.MONGODB_URI : localMongoURI;
+// MongoDB connection string
+export const MONGODB_URI = isProduction ? process.env.MONGO_URI : 'mongodb://127.0.0.1:27017/care-app';
 
 const config = { MONGODB_URI, PORT };
 export default config;
