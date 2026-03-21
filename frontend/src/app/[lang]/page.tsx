127805f9d712f5050444c54ecd4b2ab467ee10a2
sk-phan <sukiphan97@gmail.com>
2026-03-21 16:27:03 +0200

    fix lint

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -3,9 +3,9 @@ import { BASE_URL } from '@/common/api/api-base';
 
 import { ItemType } from '@/common/types/item/item.type';
 import { LocaleType } from '../i18n/locales/locales.type';
-import HeroSection from '@/features/landing-page/sections/hero-section';
-import AboutSection from '@/features/landing-page/sections/about-section';
-import ItemsSection from '@/features/landing-page/sections/items-section';
+// import HeroSection from '@/features/landing-page/sections/hero-section';
+// import AboutSection from '@/features/landing-page/sections/about-section';
+// import ItemsSection from '@/features/landing-page/sections/items-section';
 
 const PAGE = 1;
 const LIMIT = 6;
@@ -15,8 +15,8 @@ interface Props {
 }
 
 export default async function Home(props: Props) {
-  const params = await props.params;
-  const lang = params.lang;
+  // const params = await props.params;
+  // const lang = params.lang;
 
   const response = await fetch(BASE_URL + `/items?page=${PAGE}&limit=${LIMIT}`, {
     cache: 'force-cache'
@@ -33,9 +33,9 @@ export default async function Home(props: Props) {
     throw new Error(`Items fetch failed: ${response.status} ${response.statusText} | ${body}`);
   }
 
-  const data = await response.json();
+  // const data = await response.json();
 
-  const items: ItemType[] = data.entities || [];
+  // const items: ItemType[] = data.entities || [];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
