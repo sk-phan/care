0717acf2dec883f564f52babb012c547961c31ad
sk-phan <sukiphan97@gmail.com>
2026-03-22 23:06:03 +0200

    chore: remove redundant code

--- frontend/src/app/[lang]/providers.tsx
+++ frontend/src/app/[lang]/providers.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from '@mui/material';
 import theme from '@/common/styles/mui/theme';
 
 import { NotificationProvider } from '@/common/stores/notification/notification-context';
-import StoreProvider from '../store-provider';
 import ReactQueryProvider from '../react-query-provider';
 import { ReactNode } from 'react';
 
@@ -11,11 +10,9 @@ export function Providers({ children } : { children: ReactNode }) {
     return (
         <ThemeProvider theme={theme}>
             <ReactQueryProvider>
-                <StoreProvider>
-                    <NotificationProvider>
-                        {children}
-                    </NotificationProvider>
-                </StoreProvider>
+                <NotificationProvider>
+                    {children}
+                </NotificationProvider>
             </ReactQueryProvider>
         </ThemeProvider>
     );
